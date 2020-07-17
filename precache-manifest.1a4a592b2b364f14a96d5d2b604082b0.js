cc15059d93653ae23be6ae560864808b04217548
oleksandrmoskalenko <alex_wk@ukr.net>
2020-07-17 15:26:19 +0300

    first commit

---
+++ precache-manifest.1a4a592b2b364f14a96d5d2b604082b0.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6999d93ecca1f11be7bf04c313c01637",
+    "url": "/index.html"
+  },
+  {
+    "revision": "616a0b5ecb551df360b6",
+    "url": "/static/css/2.a823ed68.chunk.css"
+  },
+  {
+    "revision": "f92b14401e71ba26fdb9",
+    "url": "/static/css/main.bf7e0d66.chunk.css"
+  },
+  {
+    "revision": "616a0b5ecb551df360b6",
+    "url": "/static/js/2.4a8c31ad.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4a8c31ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f92b14401e71ba26fdb9",
+    "url": "/static/js/main.7eb6617b.chunk.js"
+  },
+  {
+    "revision": "893f76211477c960fe2c",
+    "url": "/static/js/runtime-main.0d678f6b.js"
+  },
+  {
+    "revision": "6eca925bf655c79e4c7265a33aa4cd3c",
+    "url": "/static/media/award-winning-bg.6eca925b.png"
+  },
+  {
+    "revision": "0de751aa0466e7d8ab372653a8d90fd7",
+    "url": "/static/media/benefits-bg.0de751aa.jpg"
+  },
+  {
+    "revision": "677a6cb5760921fe6d0d7b7ab1e51ef8",
+    "url": "/static/media/best-in-class-bg.677a6cb5.png"
+  },
+  {
+    "revision": "e7abd71f3b047eb8e71868b86edf2da8",
+    "url": "/static/media/chef's-tables-bg.e7abd71f.png"
+  },
+  {
+    "revision": "fdead6479e1efc8f49fd2b2c1bea4016",
+    "url": "/static/media/client-support-dedicated.fdead647.png"
+  },
+  {
+    "revision": "c0bb8078f72d44035232979142cfff14",
+    "url": "/static/media/client-support-personal.c0bb8078.png"
+  },
+  {
+    "revision": "2dcd05aa2b2cba48a403990e80f581d9",
+    "url": "/static/media/client-support-senior.2dcd05aa.png"
+  },
+  {
+    "revision": "6f9bf2881e69c320774a1d4893fc50d4",
+    "url": "/static/media/contact-us-bg.6f9bf288.jpg"
+  },
+  {
+    "revision": "8d601ae4d114e2baac2c01b39747bf78",
+    "url": "/static/media/corp-account-bg.8d601ae4.png"
+  },
+  {
+    "revision": "d4f244de93f24e0d056a87fd85facc5c",
+    "url": "/static/media/expert-service-bg.d4f244de.png"
+  },
+  {
+    "revision": "f7cba82ee2f89980fa68e2db1845ffae",
+    "url": "/static/media/feedback-bg.f7cba82e.jpg"
+  },
+  {
+    "revision": "c39f7f62dca3ed8c312639c5f716f3da",
+    "url": "/static/media/flagship-events-bg.c39f7f62.png"
+  },
+  {
+    "revision": "3e25751883ea7aac3598867fc1cbe33a",
+    "url": "/static/media/fx-prices-bg.3e257518.jpg"
+  },
+  {
+    "revision": "b54852437a4aa46988369f919245f5ea",
+    "url": "/static/media/halal-investing-bg.b5485243.jpg"
+  },
+  {
+    "revision": "d5e5a0316280c738f3fad494af0e7de9",
+    "url": "/static/media/header-accounts-bg.d5e5a031.jpg"
+  },
+  {
+    "revision": "5e19da125c8773729774857a69dd786d",
+    "url": "/static/media/header-homepage-bg.5e19da12.jpg"
+  },
+  {
+    "revision": "d37f74b497f305c3981ed44b6f770301",
+    "url": "/static/media/header-homepage.d37f74b4.png"
+  },
+  {
+    "revision": "f8a7c894c44f4df4e9cb2f4eeac49049",
+    "url": "/static/media/header-homepage2.f8a7c894.png"
+  },
+  {
+    "revision": "1581d7b08f30e5a094692c3a74369708",
+    "url": "/static/media/header-products-bg.1581d7b0.jpg"
+  },
+  {
+    "revision": "17f4b2c51736ffb785f11a804036f30f",
+    "url": "/static/media/header-vip-acc-bg.17f4b2c5.jpg"
+  },
+  {
+    "revision": "8f17c1679a2dd85dba545d891c73f935",
+    "url": "/static/media/hospitality-bg.8f17c167.png"
+  },
+  {
+    "revision": "299169daaabdac8dbb1f1b70b3e27b4c",
+    "url": "/static/media/image-laptop-bg.299169da.png"
+  },
+  {
+    "revision": "6150e1b5a73bc380100200010362c25b",
+    "url": "/static/media/insights-bg.6150e1b5.png"
+  },
+  {
+    "revision": "682f50c3d36a4b698a09b6abad5b1961",
+    "url": "/static/media/leverage-trade.682f50c3.jpg"
+  },
+  {
+    "revision": "2f954c2aa17ecdb8135494318012b54e",
+    "url": "/static/media/market-price-bg.2f954c2a.png"
+  },
+  {
+    "revision": "6979d9076b4ff613949d9569d35db4d0",
+    "url": "/static/media/prices.6979d907.png"
+  },
+  {
+    "revision": "6f9bf2881e69c320774a1d4893fc50d4",
+    "url": "/static/media/sign-bg.6f9bf288.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "fcd305cf979a2c059483f5e449b5720c",
+    "url": "/static/media/software.fcd305cf.png"
+  },
+  {
+    "revision": "5a2fbb328374d7047dda536ddc1a6d17",
+    "url": "/static/media/start-trading-bg.5a2fbb32.jpg"
+  },
+  {
+    "revision": "8f7bdde45e61b6607cb359a4763454ef",
+    "url": "/static/media/tiers-laptop.8f7bdde4.png"
+  },
+  {
+    "revision": "257da7221703383d03e73125d7ec03dd",
+    "url": "/static/media/topycal-debates-bg.257da722.png"
+  },
+  {
+    "revision": "2875438b0242e0fdf2184de1b65d2e14",
+    "url": "/static/media/ultra-competitixe-bg.2875438b.png"
+  },
+  {
+    "revision": "923fbf175a7293f4fb96391cea89ab1e",
+    "url": "/static/media/vip-account-bg.923fbf17.png"
+  }
+]);
